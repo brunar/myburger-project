dc96114c15868825779521026d0476e9f2713138
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-18 23:26:58 +0100

    Outputting Burger Ingredients Dynamically (8-129)
    Map Ingredients and create a second Array Map for cases of double ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        }); //on 2nd map() used _ as argument and i for index, make a second array for case like doulbe cheese, doulbe meat
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
