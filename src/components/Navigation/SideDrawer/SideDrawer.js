e0c73e69338c0f797b7ecb4c74ba82bdfc52667b
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-23 17:27:07 +0100

    Reusing the Backdrop (8-149)

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,33 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            {/* 1st Way - height here is a props, coming from syle-inline logo component
+        <Aux>
+            {/* shortcut show does means show={true} but for bolean */}
+            <Backdrop show={props.open} clickedback={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                {/* 1st Way - height here is a props, coming from syle-inline logo component
                 <Logo height="11%" />
             */}
-            <div className={classes.Logo}>
-                <Logo />
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
