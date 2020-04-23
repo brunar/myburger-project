4d112059428da80e4708c25f5aff7897c2c3daeb
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-23 19:23:09 +0100

    Improving Performance (8-154)
    By implementing lifecycle method: shouldComponentUpdate in the parent Modal that wrapped orderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredientsorder)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}:</span> {props.ingredientsorder[igKey]}
-                </li>
-            );
-        });
+class OrderSummary extends Component {
+    //This could be a functional component, does not have to be a class and do not need this lifecycle method 
+    //It was only for debbug and improve performance
+    componentDidUpdate() {
+        console.log('[OrderSummary.js] DidUpdate')
+    }
 
-    return (
-        <Aux>
-            <h1>Your Order</h1>
-            <p>A delicious burger with following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredientsorder)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}:</span> {this.props.ingredientsorder[igKey]}
+                    </li>
+                );
+            });
 
-            <Button clickedBtn={props.purchaseCancelled} btnType="Danger">CANCEL</Button>
-            <Button clickedBtn={props.purchaseContinued} btnType="Success">CONTINUE</Button>
-        </Aux>
-    );
+        return (
+            <Aux>
+                <h1>Your Order</h1>
+                <p>A delicious burger with following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+
+                <Button clickedBtn={this.props.purchaseCancelled} btnType="Danger">CANCEL</Button>
+                <Button clickedBtn={this.props.purchaseContinued} btnType="Success">CONTINUE</Button>
+            </Aux>
+        );
+    };
 }
 
-export default orderSummary;
+export default OrderSummary;
