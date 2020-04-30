01b57d1ab76618782fe72bb3eefac592b080e6b2
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-30 15:37:35 +0100

    Passing Ingredients via Query Params (12-225)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -124,7 +124,18 @@ class BurgerBuilder extends Component {
         //     .catch(error => {
         //         this.setState({ loading: false, purchasing: false });
         //     })
-        this.props.history.push('/checkout');
+        //Empty Array
+        const queryParams = [];
+        //create a loop
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryParams.join('&');
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
