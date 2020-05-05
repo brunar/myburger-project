0dc2265dcbab6e67ad81c107b3a7a8a248b504aa
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-05 21:30:21 +0100

    Improving Visual Feedback (13-245)
    Show validation only if the user touched

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,7 +5,7 @@ const input = (props) => {
     let inputElement = null;
     const inputClasses = [classes.InputElement];
 
-    if (props.invalid && props.shouldValidate) {
+    if (props.invalid && props.shouldValidate && props.touchedbr) {
         inputClasses.push(classes.Invalid);
     }
 
