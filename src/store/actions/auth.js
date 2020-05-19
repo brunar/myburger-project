abe1e90d9d1e4a4e220254107e4211d86fdc88e5
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-19 21:08:00 +0100

    Authentication - Getting a Token from the Backend (18-326)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionsTypes from './actionTypes';
 
 // To set a loading state and show a spinner
@@ -21,9 +22,23 @@ export const authFail = (errorArg) => {
     };
 }
 
-export const auth = (email, password) => {
+export const auth = (emailArg, passwordArg) => {
     //Need Redux Thunk - is Async
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: emailArg,
+            password: passwordArg,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDVOE8w6W1Lh-s7Pog58XvH8GrzGc4eHXc', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 }
