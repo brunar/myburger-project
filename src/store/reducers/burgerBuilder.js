ba3f235b20f55c7b123140747bb79a5e25135443
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-19 18:22:29 +0100

    Redux Adv - Refactoring Reducers Continued (17-318)
    //Optional - Lean Switch case statment

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -15,46 +15,56 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 }
 
+const addIngredient = (state, action) => {
+    const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+    //The 2nd Argument has to be an object because is what utility.js expect
+    const updatedIngredients = updatedObject(state.ingredients, updatedIngredient);
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updatedObject(state, updatedState);
+}
+
+const removeIngredient = (state, action) => {
+    const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+    //The 2nd Argument has to be an object because is what utility.js expect
+    const updatedIngs = updatedObject(state.ingredients, updatedIng);
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updatedObject(state, updatedSt);
+}
+
+const setIngredients = (state, action) => {
+    return updatedObject(state, {
+        ingredients: {
+            salad: action.ingredientsA.salad,
+            bacon: action.ingredientsA.bacon,
+            cheese: action.ingredientsA.cheese,
+            meat: action.ingredientsA.meat,
+        },
+        totalPrice: 4, //hardcode price same as initialState on top of the page
+        error: false
+    });
+    // This is good because you can add many types of ingredients
+    // Example: ingredients: action.ingredientsA,
+    // But in this app burguer has a swicth case that is not prepared to works with new ingredients in the list
+    // Optional - To order the list, salad first and not ordering from firebase
+}
+
+const fecthIngredientsFailed = (state, action) => {
+    return updatedObject(state, { error: true });
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
-            //The 2nd Argument has to be an object because is what utility.js expect
-            const updatedIngredients = updatedObject(state.ingredients, updatedIngredient);
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updatedObject(state, updatedState);
-
-        case actionTypes.REMOVE_INGREDIENT:
-            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
-            //The 2nd Argument has to be an object because is what utility.js expect
-            const updatedIngs = updatedObject(state.ingredients, updatedIng);
-            const updatedSt = {
-                ingredients: updatedIngs,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            return updatedObject(state, updatedSt);
-        case actionTypes.SET_INGREDIENTS:
-            return updatedObject(state, {
-                ingredients: {
-                    salad: action.ingredientsA.salad,
-                    bacon: action.ingredientsA.bacon,
-                    cheese: action.ingredientsA.cheese,
-                    meat: action.ingredientsA.meat,
-                },
-                totalPrice: 4, //hardcode price same as initialState on top of the page
-                error: false
-            });
-        // This is good because you can add many types of ingredients
-        // Example: ingredients: action.ingredientsA,
-        // But in this app burguer has a swicth case that is not prepared to works with new ingredients in the list
-        // Optional - To order the list, salad first and not ordering from firebase
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return updatedObject(state, { error: true });
-        default:
-            return state;
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fecthIngredientsFailed(state, action);
+        default: return state;
     }
 }
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,34 +7,49 @@ const initialState = {
     purchased: false
 }
 
+
+const purchaseInit = (state, action) => {
+    return updatedObject(state, { purchased: false });
+}
+const purchaseBurgerStart = (state, action) => {
+    return updatedObject(state, { loading: true });
+}
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updatedObject(action.orderDataRedu, { id: action.orderId });
+    return updatedObject(state, {
+        loading: false,
+        purchased: true,
+        orders: state.orders.concat(newOrder)
+        //In the order reducer, purchaseBurgerSuccess where I store this new order,
+        //that doesn't really matter because we load orders from the server anyways when you visit the order page.
+    });
+}
+const purchaseBurgerFail = (state, action) => {
+    return updatedObject(state, { loading: false });
+}
+const fetchOrdersStart = (state, action) => {
+    return updatedObject(state, { loading: true });
+}
+const fetchOrdersSuccess = (state, action) => {
+    return updatedObject(state, {
+        orders: action.ordersRedu,
+        loading: false
+    });
+}
+const fetchOrdersFail = (state, action) => {
+    return updatedObject(state, { loading: false });
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return updatedObject(state, { purchased: false });
-        case actionTypes.PURCHASE_BURGER_START:
-            return updatedObject(state, { loading: true });
-        case actionTypes.PURCHASE_BURGER_SUCESS:
-            const newOrder = updatedObject(action.orderDataRedu, { id: action.orderId });
-            return updatedObject(state, {
-                loading: false,
-                purchased: true,
-                orders: state.orders.concat(newOrder)
-                //In the order reducer, purchaseBurgerSuccess where I store this new order,
-                //that doesn't really matter because we load orders from the server anyways when you visit the order page.
-            });
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return updatedObject(state, { loading: false });
-        case actionTypes.FETCH_ORDERS_START:
-            return updatedObject(state, { loading: true });
-        case actionTypes.FETCH_ORDERS_SUCESS:
-            return updatedObject(state, {
-                orders: action.ordersRedu,
-                loading: false
-            });
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return updatedObject(state, { loading: false });
-        default:
-            return state;
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
+        case actionTypes.FETCH_ORDERS_SUCESS: return fetchOrdersSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
+        default: return state;
     }
 }
 
