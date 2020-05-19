044a995a959f159b7746c3604ecd046d5f787fee
Bruna Radeke <brunaradeke@hotmail.com>
2020-05-19 15:36:27 +0100

    Redux Adv - Refactoring Reducers (17-317)
    Optional

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updatedObject } from '../utitily'; // Export objects need curly braces
 
 const initialState = {
     ingredients: null,
@@ -17,31 +18,26 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+            //The 2nd Argument has to be an object because is what utility.js expect
+            const updatedIngredients = updatedObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
-        case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                // This is good because you can add many types of ingredients
-                //ingredients: action.ingredientsA,
+            }
+            return updatedObject(state, updatedState);
 
-                //But in this app burguer has a swicth case that is not prepared to works with new ingredients in the list
-                //Optional - To order the list, salad first and not ordering from firebase
+        case actionTypes.REMOVE_INGREDIENT:
+            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+            //The 2nd Argument has to be an object because is what utility.js expect
+            const updatedIngs = updatedObject(state.ingredients, updatedIng);
+            const updatedSt = {
+                ingredients: updatedIngs,
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+            }
+            return updatedObject(state, updatedSt);
+        case actionTypes.SET_INGREDIENTS:
+            return updatedObject(state, {
                 ingredients: {
                     salad: action.ingredientsA.salad,
                     bacon: action.ingredientsA.bacon,
@@ -50,12 +46,13 @@ const reducer = (state = initialState, action) => {
                 },
                 totalPrice: 4, //hardcode price same as initialState on top of the page
                 error: false
-            };
+            });
+        // This is good because you can add many types of ingredients
+        // Example: ingredients: action.ingredientsA,
+        // But in this app burguer has a swicth case that is not prepared to works with new ingredients in the list
+        // Optional - To order the list, salad first and not ordering from firebase
         case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
-                error: true
-            };
+            return updatedObject(state, { error: true });
         default:
             return state;
     }
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updatedObject } from '../utitily';
 
 const initialState = {
     orders: [],
@@ -9,49 +10,29 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            }
+            return updatedObject(state, { purchased: false });
         case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            }
+            return updatedObject(state, { loading: true });
         case actionTypes.PURCHASE_BURGER_SUCESS:
-            const newOrder = {
-                ...action.orderDataRedu,
-                id: action.orderId
-            }
-            return {
-                ...state,
+            const newOrder = updatedObject(action.orderDataRedu, { id: action.orderId });
+            return updatedObject(state, {
                 loading: false,
                 purchased: true,
                 orders: state.orders.concat(newOrder)
                 //In the order reducer, purchaseBurgerSuccess where I store this new order,
                 //that doesn't really matter because we load orders from the server anyways when you visit the order page.
-            }
+            });
         case actionTypes.PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading: false
-            }
+            return updatedObject(state, { loading: false });
         case actionTypes.FETCH_ORDERS_START:
-            return {
-                ...state,
-                loading: true
-            }
+            return updatedObject(state, { loading: true });
         case actionTypes.FETCH_ORDERS_SUCESS:
-            return {
-                ...state,
+            return updatedObject(state, {
                 orders: action.ordersRedu,
                 loading: false
-            }
+            });
         case actionTypes.FETCH_ORDERS_FAIL:
-            return {
-                ...state,
-                loading: false
-            }
+            return updatedObject(state, { loading: false });
         default:
             return state;
     }
