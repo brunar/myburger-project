d256009ee423f522e9f0ad07d292e6ffbaed6cf7
Bruna Radeke <brunaradeke@hotmail.com>
2020-06-30 00:10:14 +0100

    Hooks - Working with useSelector() and useDispatch() (27-464)
    Review it's a good example of useCallback to fix infinite request and avoid infinite loop, also review it in the previous chapter (26)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux'; //Ex It replaces the connect HOC from React-Redux
 
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
@@ -19,8 +19,31 @@ export const BurgerBuilder = props => {
     // } 
     const [purchasing, setPurchasing] = useState(false);
 
+    //useSelector is relative of mapStateToProps
+    const ings = useSelector(state => {
+        return state.burgerBuilderSS.ingredients
+    });
+    const pprice = useSelector(state => state.burgerBuilderSS.totalPrice);
+    const error = useSelector(state => state.burgerBuilderSS.error);
+    const isAuthenticated = useSelector(state => state.authSS.token !== null);
+
+    //useDispatch is relative of mapDispatchToProps
+    const dispatch = useDispatch();
+    const onIngredientAdded = (ingName) => dispatch(actions.addIngredient(ingName));
+    const onIngredientRemoved = (ingName) => dispatch(actions.removeIngredient(ingName));
+
+    // useCallback here is crucial to avoid infinite loop
+    // we ensure that on init ingredients actually will cache the value. This function here and
+    // not recreate this functional object for every recreation or every rendering of this component
+    const onInitIngredients = useCallback(
+        () => dispatch(actions.initIngredients()),
+        [dispatch]);
+
+    const onInitPurchase = () => dispatch(actions.purchaseInit());
+    const onSetAuthRedirectPath = (path) => dispatch(actions.setAuthRedirectPath(path));
+
     //componentDidMount lifecycle method to fetching the data
-    const { onInitIngredients } = props;
+    //const { onInitIngredients } = props; //can be removed because the props won't be via connect, but useDispatch
     useEffect(() => {
         //console.log(onInitIngredients);
         onInitIngredients();
@@ -40,10 +63,10 @@ export const BurgerBuilder = props => {
     }
 
     const purchaseHandler = () => {
-        if (props.isAuthenticated) {
+        if (isAuthenticated) {
             setPurchasing(true);
         } else {
-            props.onSetAuthRedirectPath('/checkout');
+            onSetAuthRedirectPath('/checkout');
             props.history.push('/auth');
         }
     }
@@ -52,13 +75,13 @@ export const BurgerBuilder = props => {
     }
 
     const purchaseContinueHandler = () => {
-        props.onInitPurchase();
+        onInitPurchase();
         props.history.push('/checkout');
     }
 
 
     const disabledInfo = {
-        ...props.ings
+        ...ings
     };
     for (let key in disabledInfo) {
         disabledInfo[key] = disabledInfo[key] <= 0
@@ -66,25 +89,25 @@ export const BurgerBuilder = props => {
 
     let orderSummary = null;
 
-    let burger = props.error ? <p>Ingredients can not be loaded!</p> : <Spinner />
+    let burger = error ? <p>Ingredients can not be loaded!</p> : <Spinner />
 
-    if (props.ings) {
+    if (ings) {
         burger = (
             <Aux>
-                <Burger ingredients={props.ings} />
+                <Burger ingredients={ings} />
                 <BuildControls
-                    ingredientAdded={props.onIngredientAdded}
-                    ingredientRemoved={props.onIngredientRemoved}
+                    ingredientAdded={onIngredientAdded}
+                    ingredientRemoved={onIngredientRemoved}
                     disabled={disabledInfo}
-                    purchasebr={updatePurchaseState(props.ings)}
+                    purchasebr={updatePurchaseState(ings)}
                     ordered={purchaseHandler}
-                    isAuth={props.isAuthenticated}
-                    price={props.pprice} />
+                    isAuth={isAuthenticated}
+                    price={pprice} />
             </Aux>
         );
         orderSummary = <OrderSummary
-            ingredientsorder={props.ings}
-            price={props.pprice}
+            ingredientsorder={ings}
+            price={pprice}
             purchaseCancelled={purchaseHandlerCancel}
             purchaseContinued={purchaseContinueHandler} />;
     }
@@ -99,26 +122,5 @@ export const BurgerBuilder = props => {
     );
 
 }
-const mapStateToProps = state => {
-    // burgerBuilderSS was declared at combineReducers on Index.js/App
-    return {
-        ings: state.burgerBuilderSS.ingredients,
-        pprice: state.burgerBuilderSS.totalPrice,
-        error: state.burgerBuilderSS.error,
-        isAuthenticated: state.authSS.token !== null
-    };
-}
-const mapDispatchToProps = dispatch => {
-    return {
-        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
-        onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit()),
-        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
-    }
-}
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withErrorHandler(BurgerBuilder, axios));
+export default withErrorHandler(BurgerBuilder, axios);
